5633a6098af1b88e9582d57ad0721d0b6efb0353
Manuel Alejandro <mplaza005@gmail.com>
2025-06-03 14:50:46 -0500

    first commit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        return response()->json($categories);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required|max:255',
+        ]);
+
+        $category = Category::create($request->all());
+
+        return response()->json($category);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id) //si se pasa $id se utiliza la comentada
+    {
+        $category = Category::findOrFail($id);
+        // $category = Category::with(['posts.user'])->findOrFail($id);
+        // $category = Category::with(['posts'])->findOrFail($id);
+        return response()->json($category);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+// Route::get('categories', [CategoryController::class,'index'])->name('api.v1.categories.index');
+// Route::post('categories', [CategoryController::class,'store'])->name('api.v1.categories.store');
+// Route::get('categories/{category}', [CategoryController::class,'show'])->name('api.v1.categories.show');
+
+Route::resource('categories',CategoryController::class);
