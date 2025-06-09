46da96aed9058e3d4fc7d35f2b9e4a2d1b481f6f
Manuel Alejandro <mplaza005@gmail.com>
2025-06-09 16:51:46 -0500

    category included complete

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Builder;
 class Category extends Model
 {
     protected $fillable = ['name']; //Campos que se van a asignacion masiva:
-
+    protected $allowIncluded = ['posts', 'posts.user']; //las posibles Querys que se pueden realizar
 
     //relacion uno a muchos
     public function posts()
@@ -19,14 +19,27 @@ class Category extends Model
 
     public function scopeIncluded(Builder $query)
     {
+        if (empty($this->allowIncluded) || empty(request('included'))) { // validamos que la lista blanca y la variable included enviada a travez de HTTP no este en vacia.
+            return;
+        }
 
-        $relations = explode(',', request('included'));
+
+        $relations = explode(',', request('included')); //['posts','relation2']//recuperamos el valor de la variable included y separa sus valores por una coma
+
+        //return $relations;
+
+        $allowIncluded = collect($this->allowIncluded); //colocamos en una colecion lo que tiene $allowIncluded en este caso = ['posts','posts.user']
+
+        foreach ($relations as $key => $relationship) { //recorremos el array de relaciones
+
+            if (!$allowIncluded->contains($relationship)) {
+                unset($relations[$key]);
+            }
+        }
+
+        // return $relations;
 
         $query->with($relations); //se ejecuta el query con lo que tiene $relations en ultimas es el valor en la url de included
 
     }
-
-
-
-
 }
